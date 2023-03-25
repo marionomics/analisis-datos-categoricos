780b29044d0230008e0eae3bf0be3df7c0936224
Mario A. Garcia-Meza <marius.agm@gmail.com>
2023-03-25 15:29:24 -0600

    added gitignore

--- R/titanic.R
+++ R/titanic.R
@@ -40,3 +40,22 @@ calcular_prob <- function(coeficientes, segunda = 0, tercera = 0){
 }
 
 calcular_prob(titanic_logit$coefficients, 0, 1)
+
+
+head(titanic)
+
+titanic %>%
+    ggplot(aes(x = Age))+
+    geom_density()
+
+titanic_logit <- glm(Survived ~ factor(Pclass) + Sex + Age + SibSp,
+                    family = binomial(),
+                    data = titanic)
+summary(titanic_logit)
+
+
+calcular_prob <- function(coeficientes, segunda = 0, tercera = 0, hombre = 0, edad, sibsp = 0){
+    odds <- exp(coeficientes[1] + coeficientes[2]* segunda + coeficientes[3]*tercera )
+    probabilidad = odds/(1+odds)
+    return(probabilidad)
+}
